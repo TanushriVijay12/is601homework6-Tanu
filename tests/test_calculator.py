d9f3a2609c5fef5fe04957405506a36b4ce08f48
TanushriVijay12 <tanushri.fall24@gmail.com>
2025-02-11 22:46:46 -0500

    made some test cases

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,17 @@
-'''My Calculator Test'''
 from calculator import Calculator
 
 def test_addition():
-    '''Test that addition function works '''    
-    assert Calculator.add(2,2) == 4
+    '''Test that addition function works'''
+    assert Calculator.add(15, 25) == 40
 
 def test_subtraction():
-    '''Test that addition function works '''    
-    assert Calculator.subtract(2,2) == 0
+    '''Test that subtraction function works'''
+    assert Calculator.subtract(100, 60) == 40
 
 def test_divide():
-    '''Test that division function works '''    
-    assert Calculator.divide(2,2) == 1
+    '''Test that division function works'''
+    assert Calculator.divide(48, 8) == 6
 
 def test_multiply():
-    '''Test that multiply function works '''    
-    assert Calculator.multiply(2,2) == 4
+    '''Test that multiplication function works'''
+    assert Calculator.multiply(4, 9) == 36
